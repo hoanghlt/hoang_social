dbb603d1a46c206f2927d19e006c46f5dc7e7e2e
hoanghlt <4hoangphuson15031995@gmail.com>
2022-01-05 22:31:14 +0700

    handle error middleware vs create logger file vs create schema users

--- src/modules/index/index.controller.ts
+++ src/modules/index/index.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-export default class IndexController{
+export default class IndexController {
     public index = (req: Request, res: Response, next: NextFunction) => {
         try {
             res.status(200).send('API is running...');
